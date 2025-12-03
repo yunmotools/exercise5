8efefbfa5a46158523998fb7d4a4a5ace037021d
jordonjones <jordon01jones@gmail.com>
2025-12-03 12:43:02 -0600

    exercise5

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,18 @@
 public class Main {
+    static String first;
+    static char middle;
+    static String last;
+    static String full;
 
-}
+    public static void main(String[] args) {
+        first = "Jordon";
+        middle = 'Y';
+        last = "Jones";
+        full = (first + " " + middle + " " + last);
+        System.out.println(full);
+
+
+    }
+
+    }
+
